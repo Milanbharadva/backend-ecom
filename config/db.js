849e0076a5214d3cfac449443205895a654f3377
Milanbharadva <96285463+Milanbharadva@users.noreply.github.com>
2024-12-13 21:11:04 +0530

    Password changed

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://Milan2004:fOOC8xSteoHdcP0U@clusteradmin.pe0pcrv.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=ClusterAdmin", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...');
+    await mongoose.connect(
+      "mongodb+srv://Milan2004:Milan2004@clusteradmin.pe0pcrv.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=ClusterAdmin",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB Connected...");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
